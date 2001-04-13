631ce88b071121e35e92de4669de552a564bced6
Don Baccus <dhogaza@pacifier.com>
2001-04-13 18:37:18 +0000

    Just reorganizing more datamodel files into the new scheme, also made the one-line change necessary to port the "page" module.

---
+++ sql/postgresql/page-create.sql
@@ -0,0 +1,15 @@
+--
+-- packages/acs-page/sql/page-create.sql
+--
+-- @author rhs@mit.edu
+-- @creation-date 2000-09-29
+-- @cvs-id $Id$
+--
+
+create table pages (
+    package_id		integer constraint pages_package_id_fk
+                        references apm_packages (package_id)
+                        constraint pages_package_id_pk
+                        primary key,
+    content		text not null
+);
